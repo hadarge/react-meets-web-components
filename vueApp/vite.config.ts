d1d0e64daf7482f0c67bcb49576718864041f007
Hadar Geva <hadar111@gmail.com>
2025-07-09 00:33:07 +0300

    fix vue vite.config.ts

--- vueApp/vite.config.ts
+++ vueApp/vite.config.ts
@@ -3,7 +3,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [vue({
+        template: {
+            compilerOptions: {
+                isCustomElement: tag => tag === 'like-button' || tag === 'video-player'
+            }
+        }
+    })],
     server: {
         cors: true,
         port: 3333
